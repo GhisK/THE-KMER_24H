3cb146c2df737be45223b273eae109d3dd322983
YvesTAGNY <yves.tagny@yahoo.fr>
2017-01-21 17:41:51 +0100

    modification du controlleur de l'interface joueur

--- 1024Bornes/src/Controlleur/ControlleurIJ.java
+++ 1024Bornes/src/Controlleur/ControlleurIJ.java
@@ -1,6 +1,155 @@
 package Controlleur;
 
-public class ControlleurIJ {
+import java.io.IOException;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+public class ControlleurIJ {
 	
+	@FXML private Pane pj1;
+	@FXML private Pane pj2;
+	@FXML private Pane pj3;
+	@FXML private Pane pj4;
+
+	@FXML private Button bj11;
+	@FXML private Button bj12;
+	@FXML private Button bj13;
+	@FXML private Button bj14;
+	@FXML private Button bj15;
+	@FXML private Button bj16;
+	
+	@FXML private Button bj21;
+	@FXML private Button bj22;
+	@FXML private Button bj23;
+	@FXML private Button bj24;
+	@FXML private Button bj25;
+	@FXML private Button bj26;
+	
+	@FXML private Button bj31;
+	@FXML private Button bj32;
+	@FXML private Button bj33;
+	@FXML private Button bj34;
+	@FXML private Button bj35;
+	@FXML private Button bj36;
+	
+	@FXML private Button bj41;
+	@FXML private Button bj42;
+	@FXML private Button bj43;
+	@FXML private Button bj44;
+	@FXML private Button bj45;
+	@FXML private Button bj46;
+	
+	@FXML private Label nj1;
+	@FXML private Label nj2;
+	@FXML private Label nj3;
+	@FXML private Label nj4;
+	
+	/**/
+	@FXML
+	protected void dobj11(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj12(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj13(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj14(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj15(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj16(ActionEvent event) throws IOException {
+		
+	}
+	
+	
+	/**/
+	@FXML
+	protected void dobj21(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj22(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj23(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj24(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj25(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj26(ActionEvent event) throws IOException {
+		
+	}
+	
+	/**/
+	@FXML
+	protected void dobj31(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj32(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj33(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj34(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj35(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj36(ActionEvent event) throws IOException {
+		
+	}
+	
+	/**/
+	@FXML
+	protected void dobj41(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj42(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj43(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj44(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj45(ActionEvent event) throws IOException {
+		
+	}
+	@FXML
+	protected void dobj46(ActionEvent event) throws IOException {
+		
+	}
 }
